2dca1de86c3c6c406cc5173d453dc5e3266e2247
Luca-Pritchard <160200906+Luca-Pritchard@users.noreply.github.com>
2025-06-13 11:07:35 +1000

    skibid i why wont flask work ts pmo icl

--- app/routes.py
+++ app/routes.py
@@ -3,41 +3,4 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Lebron'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
-
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Lebron'}
-    return render_template('index.html', title='Home', user=user)
-
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return 'Hello, World!'
