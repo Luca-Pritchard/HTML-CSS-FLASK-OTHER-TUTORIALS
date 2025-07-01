b33223af80f19410e7eb0c1f09d266ba6bae53dc
Luca-Pritchard <160200906+Luca-Pritchard@users.noreply.github.com>
2025-07-01 12:11:43 +1000

    aaaa

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,27 @@
-from flask import render_template
 from app import app
 from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
-# ...
-
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Chad'}
+    user = {'username': 'Miguel'}
     posts = [
         {
-            'author': {'username': 'Sub-5 black piller'},
-            'body': 'Its all over for me'
+            'author': {'username': 'Sub-5'},
+            'body': 'Im so Chuzz! There is no hope for me :('
         },
         {
-            'author': {'username': 'Weakest white piller'},
-            'body': 'You may be cooked, but thats ok. you must stay strong and make the most of your situation king.'
+            'author': {'username': 'King henry'},
+            'body': 'You must stay strong king'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
